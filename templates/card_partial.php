5068a1e5e710d56e0364481d303ec2fc3bf08828
Fiddykhoum <fiddy@orange.fr>
2023-02-18 16:21:55 +0100

    add delete button for admin only

--- templates/card_partial.php
+++ templates/card_partial.php
@@ -1,3 +1,7 @@
+<?php 
+require_once "./lib/tools.php"
+?>
+
 <div class="col-md-4">
   <div class="card">
       <img src="<?=getCardImage($card['image']); ?>" 
@@ -10,15 +14,20 @@
       <a href="carte.php?id=<?=$card['id']; ?>" class="btn btn-primary">Détails</a>
       <?php
         $level = getLevel();
-        if ($level == 1) { 
-          //zone echo et var_dump
-          echo $card['id'];
-          ?>
-            <!-- declencher une fonction delete (query) à partir de ce bouton -->
-            <!-- recupérer l'id de la card et l'utliser pour une query sql qui suppeimera les données de la db -->
-            <a href="carte.php?id=<?=$card['id']; ?>" class="btn btn-primary" onclick="delete(<?php $card['id'] ?>)">Supprimer</a>
+        if ($level == 1) {     
+               
+          if(isset($_POST[$card['id']])) {
+            $pdo = new PDO('mysql:dbname=studi_live_cuisinea;host=127.0.0.1;charset=utf8mb4', 'root', '');
+
+            $delquery = " DELETE FROM recipes WHERE recipes.id = " . $card['id'] .";";
+            echo "cette carte a été supprimée de la base de données, Vous pouvez actualiser";
+            $query = $pdo->query($delquery);
+           } ?>
+          <form method="post">
+            <input type="submit"  name="<?=$card['id']; ?>" value="Supprimer"/> 
+          </form>
 
         <?php } ?>
       </div>
   </div>
-</div>
+  </div> 
