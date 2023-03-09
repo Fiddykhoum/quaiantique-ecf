ed45146d12116d8f8f1d630145b36cab956bcf7c
Fiddykhoum <fiddy@orange.fr>
2023-03-09 04:27:29 +0100

    move js script in assets/js/script.js files

--- reserver.php
+++ reserver.php
@@ -48,43 +48,8 @@
     </div>
   </div>
 </div>
-<!-- <script src="./assets/js/script.js"> -->
-<script>
-    const midiDiv = document.getElementById('midiDiv');
-    const soirDiv = document.getElementById('soirDiv');
+<script type="text/javascript" src="./assets/js/script.js"></script>
 
-    // listen date input
-    const date = document.getElementById("date") 
-    
-    // after select date, show resMidi or resSoir
-    date.addEventListener('input', function() {
-
-      // split date delete "-" ==> format  ['2023', '03', '15']
-      var myDate = date.value.split("-");
-      var newDate = new Date( myDate[0], myDate[1] - 1, myDate[2]);
-      $timestamp = newDate.getTime();
-      const day = newDate.getDay($timestamp);
-      console.log(day);
-      if (day === 1) {
-        // montrer que le soir
-        midiDiv.style.display = 'none';
-        soirDiv.style.display = 'block';
-        alert('Ouvert que le soir');
-
-      } else if ( day === 0) {
-        //montrer que le matin
-        soirDiv.style.display = 'none';
-        midiDiv.style.display = 'block';
-        alert('Ouvert que le midi');
-
-      } else {
-        //Monter les 2
-        console.log('les 2');
-        midiDiv.style.display = 'block';
-        soirDiv.style.display = 'block';
-      }
-    })
-</script>
 <?php 
 require_once('templates/footer.php');
 ?>
